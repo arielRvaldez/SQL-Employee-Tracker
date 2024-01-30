673fb4de02bda1513d89cfda63bb0cfab708fe23
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-01-29 22:19:33 -0600

    installed node.js

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'black',
+    // MySQL username,
+    user: 'root',
+    password: 'Ariel8290',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the Employee database.`)
+);
+
+// Create a movie
+app.post('/api/new-employee', ({ body }, res) => {
+  const sql = `INSERT INTO employees (employee_name)
+    VALUES (?)`;
+  const params = [body.employee_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Read all movies
+app.get('/api/employees', (req, res) => {
+  const sql = `SELECT id, employee_name AS title FROM employees`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Delete a movie
+app.delete('/api/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employees WHERE id = ?`;
+  const params = [req.params.id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+      message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// BONUS: Update review name
+app.put('/api/employee/:id', (req, res) => {
+  const sql = `UPDATE employee SET employee = ? WHERE id = ?`;
+  const params = [req.body.employee, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
