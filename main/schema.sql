401676ef0a2f3935edbdf951adc193f8876bf751
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-02-01 22:36:24 -0600

    fixed schema

--- main/schema.sql
+++ main/schema.sql
@@ -4,17 +4,18 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    dept_name VARCHAR(30) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    budget MONEY DEFAULT 100000
 );
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id)
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id),
     ON DELETE SET NULL
 );
 
@@ -22,13 +23,12 @@ CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) 
     REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) 
-    REFERENCES employee(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
     ON DELETE SET NULL  
 );
   
