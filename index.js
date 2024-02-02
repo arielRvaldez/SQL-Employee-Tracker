9adf095b72e4f8fa7bbfb05334c2f0f794ea6654
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-02-02 16:31:03 -0600

    debug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require("./db");
+const db = require("./main/db");
 
 // Display logo text, load main prompts
 function init() {
