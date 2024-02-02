68534c0dd8b05bc92276b965f8183b728fcdfd9f
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-02-01 21:14:01 -0600

    added more modules

---
+++ README.md
@@ -0,0 +1,43 @@
+# SQL-Employee-Tracker
+
+## Description:
+A user enters their choices in command line application to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+## Table of Contents:
+- [Usage](#Usage)
+- [Install](#Install)
+- [What I Learned](#What-I-Learned)
+- [Screenshot](#Screeshot)
+- [Walkthrough Video](#Walkthrough-Video)
+- [Author](#Author)
+- [Acknowledgments](#Acknowledgments)
+
+## Usage
+```md
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+```
+## Install
+1. Node.js [Version 20.11.0 LTS] https://nodejs.org/en/
+2. MySQL2 [Version 3.9.1] https://www.npmjs.com/package/mysql2
+3. Inquirer.js [Version 8.2.4](https://www.npmjs.com/package/inquirer/v/8.2.4)
+4. Splash Screen [Version 0.2.7] https://www.npmjs.com/package/asciiart-logo
+
+## What I Learned
+- To build a Node.js command-line application that takes in user input to create a database to add and view employees and assign ids, roles, salaries, departments, managers, and department utilized budget. The application prompts the user to select an employee or department, a function to be completed i.e. add, remove, or update , and saves the database in a table format. 
+
+## Screenshot
+The following picture shows application example of logo:
+![](./examples/logo-sample.jpeg)
+
+## Walkthrough Video:
+Here is a <a href="https://drive.google.com/file/d/1tDCwJYoW2IlsRZp58e2KCShYssqWBdxI/view" target="_blank">Link</a> to a video walkthrough that demonstrates the functionality of the application.
+
+## Author
+Here is my <a href="https://github.com/arielRvaldez/SQL-Employee-Tracker.git" target="_blank">Github Repo Link</a>.
+
+## Acknowledgements
+- NPM References https://www.npmjs.com/package/promise-mysql
+- Youtube https://www.youtube.com/watch?v=boaSDVO-5wQ
+- Calendy Tutor
---
+++ main/db/index.js
@@ -0,0 +1,122 @@
+const mysql = require("mysql2");
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Ariel8290",
+  database: "employees_database",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Get a promise-based connection from the pool
+const connection = pool.promise();
+
+// const connection = require("./connection");
+
+class DB {
+  //  connect class 
+  constructor(connection) {
+    this.connection = connection;
+  }
+  // find all employees
+  findAllEmployees() {
+    return this.connection.promise().query(
+      "SELECT * from employee"
+    );
+  }
+  // find all managers
+  findAllPossibleManagers(employeeId) {
+    return this.connection.promise().query(
+      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+      employeeId
+    );
+  }
+  // create new employee
+  createEmployee(employee) {
+    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+  }
+  // remove employee with id
+  removeEmployee(employeeId) {
+    return this.connection.promise().query(
+      "DELETE FROM employee WHERE id = ?",
+      employeeId
+    );
+  }
+  // update employee's role
+  updateEmployeeRole(employeeId, roleId) {
+    return this.connection.promise().query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
+  // update employee's manager
+  updateEmployeeManager(employeeId, managerId) {
+    return this.connection.promise().query(
+      "UPDATE employee SET manager_id = ? WHERE id = ?",
+      [managerId, employeeId]
+    );
+  }
+  // find all roles with the departments
+  findAllRoles() {
+    return this.connection.promise().query(
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    );
+  }
+  // create new role
+  createRole(role) {
+    return this.connection.promise().query("INSERT INTO role SET ?", role);
+  }
+  // remove role with id
+  removeRole(roleId) {
+    return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
+  }
+  // list all departments
+  findAllDepartments() {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name FROM department;"
+    );
+  }
+  // list departments with employees, their roles, and utilized department budget
+  viewDepartmentBudgets() {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+    );
+  }
+  // create new department
+  createDepartment(department) {
+    return this.connection.promise().query("INSERT INTO department SET ?", department);
+  }
+  // remove department with id
+  removeDepartment(departmentId) {
+    return this.connection.promise().query(
+      "DELETE FROM department WHERE id = ?",
+      departmentId
+    );
+  }
+  // find employees by department with role titles
+  findAllEmployeesByDepartment(departmentId) {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+      departmentId
+    );
+  }
+  // find employees by manager, with departments and roles titles
+  findAllEmployeesByManager(managerId) {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      managerId
+    );
+  }
+}
+module.exports = new DB(connection);
+
+
+
+
+
+
+
+
