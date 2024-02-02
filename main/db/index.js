969780fb6893bc4d3af9d93cba43b55dde6b45b5
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-02-01 20:29:11 -0600

    added READ.me and asciiart-logo

--- main/db/index.js
+++ main/db/index.js
@@ -56,13 +56,13 @@ class DB {
   removeRole(roleId) {
     return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
   }
-  // find all departments
+  // list all departments
   findAllDepartments() {
     return this.connection.promise().query(
       "SELECT department.id, department.name FROM department;"
     );
   }
-  // find all departments with employees, their roles, and utilized department budget
+  // list departments with employees, their roles, and utilized department budget
   viewDepartmentBudgets() {
     return this.connection.promise().query(
       "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
