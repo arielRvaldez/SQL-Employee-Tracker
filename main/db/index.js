6750e575b188f7144e5534e2a5d9edf936539a85
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-02-02 15:15:22 -0600

    fixed connection

--- main/db/index.js
+++ main/db/index.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Ariel8290",
-  database: "employees_database",
+  database: "employees_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
